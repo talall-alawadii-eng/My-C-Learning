b76bfa6e785c794956ce8952699043459c1adb81
talall-alawadii-eng <talall.alawadii.eng@gmail.com>
2026-06-14 15:39:13 +0300

    Create first_code.c
    Add first C code assignment

---
+++ first_code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("Talall Alawadii first code \n");
+    int a=10;
+    int b=15;
+    printf("The value of a is %d and the value of b is %d\n",a,b);
+    float x=9.30;
+    float y=8.88;
+    printf("The value of float x is %.2f and the value of float y is %.2f\n",x,y);
+    char c='T';
+    printf("My letter is (%c)\n",c);
+    char t[6]="Talal";
+    printf("My name is : %s\n",t);
+    printf("The last later of my name:(%c)\n",t[4]);
+    return 0;
+}
