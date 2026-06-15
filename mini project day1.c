ea2b84681782893fd63b763a34174af54885931a
talall-alawadii-eng <talall.alawadii.eng@gmail.com>
2026-06-15 21:43:09 +0300

    Create mini project day1.c

---
+++ mini project day1.c
@@ -0,0 +1,10 @@
+int main() {
+    // Write C code here
+    float c,f;
+    printf("Enter tempreature in celsius:");
+    scanf("%f",&c);
+    f=(9.0/5)*c+32;
+    printf("Temp in fahrenheit:%0.3f",f);
+
+    return 0;
+}
