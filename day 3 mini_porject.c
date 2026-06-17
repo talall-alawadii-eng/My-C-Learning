2149f1af5002bd11ea6bc1e60dd1f88237810ea4
talall-alawadii-eng <talall.alawadii.eng@gmail.com>
2026-06-17 21:13:34 +0300

    Update day 3 mini_porject.c

--- day 3 mini_porject.c
+++ day 3 mini_porject.c
@@ -41,3 +41,54 @@ int main() {
 
     return 0;
 }
+====================================================
+//usig switch
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+   
+    char x;
+    float r;
+    float l;
+    float w;
+    float a;
+    printf("please enter the letter for the shape u want:");
+    scanf("%c",&x);
+    switch(x){
+        case 'r':
+        case 'R':
+            printf("your shape is rectangle please enter the following:\n");
+            printf("Lenght=");
+            scanf("%f",&l);
+            printf("Width=");
+            scanf("%f",&w);
+            a=w*l;
+            printf("Area of rectangle=%0.2f",a);
+        break;// break=else
+        case 'c':
+        case 'C':
+            printf("your shape is circle please enter the following:\n");
+            printf("Radius=");
+            scanf("%f",&r);
+            a=3.141592654*r*r;
+            printf("Area of circle=%0.4f",a);
+        break;
+        case 't':
+        case 'T':
+            printf("your shape is right angle triangle please enter the following:\n");
+            printf("Base=");
+            scanf("%f",&w);
+            printf("Hight=");
+            scanf("%f",&l);
+            a=0.5*w*l;
+            printf("Area of triangle=%0.2f",a);
+         break;
+         default: 
+            printf("Invalid shape!!");
+            break;
+}
+
+
+    return 0;
+}
